75b288fb403346843c73238f18fb8274f29dfec5
root <root@ubuntu22.04>
2025-08-15 08:36:01 +0000

    Cambios 0115082015

--- app/controllers/movimiento_controller.py
+++ app/controllers/movimiento_controller.py
@@ -1,9 +1,5 @@
-# app/controllers/movimiento_controller.py
-
 from app.models.movimiento import Movimiento
-from datetime import datetime
 
-# Simulación de base de datos en memoria
 db = []
 
 def listar_movimientos():
